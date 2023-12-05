9b1fc861a166a4416d77eada0a8d5d071316b763
villethan720 <93165624+villethan720@users.noreply.github.com>
2023-12-04 19:39:23 -0500

    Add files via upload

---
+++ PWA final project/service-worker.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = 'input-output-pwa-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/home.html',
+    '/manifest.json',
+    './stylesheets/styles.css',
+    './stylesheets/style.css',
+    './Js/app.js',
+    './Images/audi.png'
+];
+
